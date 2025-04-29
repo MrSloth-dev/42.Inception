034fdbfbd8896e610b730a0878c48d2fb48e915e
MrSloth-dev <joao.a.barbosa@proton.me>
2025-04-29 17:30:26 +0100

    something is wrong with volumes

--- srcs/requirements/portainer/tools/setup.sh
+++ srcs/requirements/portainer/tools/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
+if [ ! -w "/var/run/docker.sock" ]; then
+    echo "Warning: Docker socket is not writable. Trying to fix permissions..."
+    chmod 666 /var/run/docker.sock || echo "Failed to change permissions on Docker socket"
+fi
 echo "Starting Portainer..."
 exec /usr/local/bin/portainer \
   --data /data \
-  --bind=:9000 \
-  --assets /opt/portainer/public
