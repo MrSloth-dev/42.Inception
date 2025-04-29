42f444450dbaa8209aaf74c5d3ab81d3f8aea873
MrSloth-dev <joao.a.barbosa@proton.me>
2025-04-29 14:05:43 +0100

    Add handle to `docker compose` and `docker-compose` and secret creator

---
+++ se.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+mkdir -p secrets
+USER=$(whoami)
+PASS=${1:-password}
+DOMAIN_NAME=$USER.42.fr
+WP_URL=https://"$USER".42.fr
+WP_HOME=https://"$USER".42.fr
+WP_DB_NAME=mariadb
+WP_ADMIN_EMAIL="$USER"@student.42porto.com
+WP_DB_HOST="$USER"42
+DB_USER="$USER"42
+WP_ADMIN_USER=superuser
+DB_ROOT_PASSWORD="$PASS"
+WP_ADMIN_PASSWORD="$PASS"
+DB_PASSWORD="$PASS"
+DB_HOST=mariadb
+REDIS_PASSWORD="$PASS"
+FTP_USER="$USER"42
+FTP_PASSWORD="$PASS"
+FTP_PASV_ADDRESS=127.0.0.1
+echo "$DOMAIN_NAME" > ./secrets/domain_name
+echo "$WP_URL" > ./secrets/wp_url
+echo "$WP_HOME" > ./secrets/wp_home
+echo "$WP_DB_NAME" > ./secrets/wp_db_name
+echo "$WP_ADMIN_EMAIL" > ./secrets/wp_admin_email
+echo "$WP_DB_HOST" > ./secrets/wp_db_host
+echo "$DB_USER" > ./secrets/db_user
+echo "$WP_ADMIN_USER" > ./secrets/wp_admin_user
+echo "$DB_ROOT_PASSWORD" > ./secrets/db_root_password
+echo "$WP_ADMIN_PASSWORD" > ./secrets/wp_admin_password
+echo "$DB_PASSWORD" > ./secrets/db_password
+echo "$DB_HOST" > ./secrets/db_host
+echo "$REDIS_PASSWORD" > ./secrets/redis_password
+echo "$FTP_USER" > ./secrets/ftp_user
+echo "$FTP_PASSWORD" > ./secrets/ftp_password
+echo "$FTP_PASV_ADDRESS" > ./secrets/ftp_pasv_address
+
